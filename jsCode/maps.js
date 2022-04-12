c0088f75cf32966f038b77f316014a0a4cd2c639
Mitansh Chaudhari <93405512+MitChaudhari@users.noreply.github.com>
2022-04-11 20:17:30 -0500

    Adding the maps using API

--- jsCode/maps.js
+++ jsCode/maps.js
@@ -1,8 +1,7 @@
-function initMap(){
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-  }
-  
-  google.maps.event.addDomListener(window, 'load', initMap);
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
